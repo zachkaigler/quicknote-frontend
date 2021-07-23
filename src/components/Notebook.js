44260db787351d70be88761e096820f354350c01
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-07-23 18:55:28 -0400

    Refactor

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,73 +1,38 @@
-import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { Redirect, useHistory } from "react-router-dom"
-import { baseUrl } from "../baseurl"
+import { useHistory } from "react-router-dom"
 
 const Notebook = () => {
-    const [isLoaded, setIsLoaded] = useState(false)
     const user = useSelector(state => state.userReducer.user)
     const notes = useSelector(state => state.userReducer.notes)
     const dispatch = useDispatch()
     const history = useHistory()
-
-    useEffect(() => {
-        if (!user) {
-            if (localStorage.qnToken) {
-                console.log(localStorage.qnUserId)
-                fetch(`${baseUrl}/users/authenticate`, {
-                    method: "GET",
-                    headers: {
-                        "Authorization": `Bearer: ${localStorage.qnToken}`
-                    }
-                })
-                .then(resp => resp.json())
-                .then(data => {
-                        if (data.error) {
-                            console.log(data.error)
-                        } else {
-                            dispatch({type: "SET_USER", payload: data.result})
-                            dispatch({type: "SET_NOTES", payload: data.result.notes})
-                            setIsLoaded(true)
-                        }
-                    }
-                )
-            } else {
-                return <Redirect to="/"/>
-            }
-        } else {
-            setIsLoaded(true)
-        }
-    }, [user, dispatch])
 
     const handleClick = () => {
         localStorage.clear()
         dispatch({type: "SET_USER", payload: null})
         history.push("/")
     }
-
-    if (isLoaded) {
-        // Break into component later on
-        const noteCards = notes.map((note) => {
-            return (
-                <div key={note._id}>
-                    <h2>{note.title}</h2>
-                    <p>{note.content}</p>
-                </div>
-            )
-        })
-    
+  
+    // Break into component later on
+    const noteCards = notes.map((note) => {
         return (
-            <div>
-                <h1>Hello, {user.firstName}</h1>
-                <div>
-                    {noteCards}
-                </div>
-                <button onClick={handleClick}>Log Out</button>
+            <div key={note._id}>
+                <h2>{note.title}</h2>
+                <p>{note.content}</p>
             </div>
         )
-    } else {
-        return null
-    }
+    })
+
+    return (
+        <div>
+            <h1>Hello, {user.firstName}</h1>
+            <div>
+                {noteCards}
+            </div>
+            <button onClick={handleClick}>Log Out</button>
+        </div>
+    )
+  
 }
 
 export default Notebook
