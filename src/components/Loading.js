f56404a6020ad2d3107e4c42d82163de1a9eae3e
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-07-28 22:45:38 -0400

    Fixed bug with loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import { ThreeDots } from "react-loading-icons"
 
 const Loading = () => {
     return (
-        <div className={`loading-${localStorage.qnTheme ? localStorage.theme : "dark"}`}>
+        <div className={`loading-${localStorage.qnTheme ? localStorage.qnTheme : "dark"}`}>
             <ThreeDots fill="#808080"/>
         </div>
     )
